81441e5df48b095f39ab654c132c3bfc7fc25faf
Julian Arni <julian@edx.org>
2013-06-06 14:44:46 -0400

    Add passive jsinput update option

--- common/static/js/capa/jsinput.js
+++ common/static/js/capa/jsinput.js
@@ -11,10 +11,11 @@
 
 
     function jsinputConstructor (spec) {
-        // Define an class that will be instantiated for each
-        // jsinput element of the DOM
+        // Define an class that will be instantiated for each.jsinput element
+        // of the DOM
 
-        // Private methods ----------------------------------------------------
+        /*                      Private methods                          */
+
         // Get the hidden input field to pass to customresponse
         function inputfield () {
             var parent = $(spec.elem).parent();
@@ -22,12 +23,19 @@
         }
 
         // Put the return value of gradefn in the hidden inputfield.
+        // If passed an argument, does not call gradefn, and instead directly
+        // updates the inputfield with the passed value.
         function update () {
-            var ans = $(spec.elem).
+            var ans;
+            if ( arguments.length > 0) {
+                ans = arguments[0];
+            } else {
+                $(spec.elem).
                     find('iframe[name^="iframe_"]').
                     get(0).      // jquery might not be available in the iframe
                     contentWindow.
                     gradefn();
+            }
             inputfield().val(ans);
             console.log("Answer: " + inputfield().val());
             return;
@@ -38,23 +46,52 @@
         function updateHandler () {
             var updatebutton = $(spec.elem).
                     find('button[class="update"]').get(0);
-
             $(updatebutton).click( update );
         }
 
-        // Public methods -----------------------------------------------------
+        if (spec.passive === false) {
+            updateHandler();
+        } else {
+            // If set up to passively receive updates (intercept a function's
+            // return value whenever the function is called) add an event
+            // listener that listens to messages that match "that"'s id.
+            // Decorate the iframe gradefn with updateDecorator.
+            iframe.contentWindow[gradefn] = updateDecorator(iframe.contentWindow[gradefn]);
+            iframe.contentWindow.addEventListener('message', function (e) {
+                var id = e.data[0];
+                var msg = e.data[1];
+                if (id == spec.id) { update(msg); }
+            });
+        }
+
+        /*                       Public methods                     */
+
         // 'that' is the object returned by the constructor. It has a single
         // public method, "update", which updates the hidden input field.
         var that = {};
         that.update = update;
 
-        updateHandler();
 
         return that;
     }
 
+    function updateDecorator (fn, id) {
+    // Simple function decorator that posts the output of a function to the
+    // parent iframe before returning the original function's value.
+    // Can be used to decorate one or more gradefn (instead of using an
+    // explicit "Update" button) when gradefn is automatically called as part
+    // of an application's natural behavior.
+    // The id argument is used to specify which of the instances of jsinput on
+    // the parent page the message is being posted to.
+        return function () {
+            var result = fn.apply(null, arguments);
+            window.parent.contentWindow.postMessage([id, result], document.referrer);
+            return result;
+        };
+    }
+
     function walkDOM () {
-        // Find all jsinput elements, and create a jsinput object for each one
+    // Find all jsinput elements, and create a jsinput object for each one
         var all = $(document).find('section[class="jsinput"]');
         var newid;
         all.each(function() {
@@ -63,7 +100,8 @@
             var elem = this ;
             var newJsElem = jsinputConstructor({
                 id: newid ,
-                elem: this
+                elem: this ,
+                passive: false 
             });
         });
     }
